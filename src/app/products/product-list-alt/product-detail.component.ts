150faf308632ced76f621c9719f8d975b4ee07e4
Robert Aliaj <robertaliaj97@gmail.com>
2023-09-21 10:54:35 +0200

    combining all the streams

--- src/app/products/product-list-alt/product-detail.component.ts
+++ src/app/products/product-list-alt/product-detail.component.ts
@@ -3,7 +3,7 @@ import {Supplier} from '../../suppliers/supplier';
 import {Product} from '../product';
 
 import {ProductService} from '../product.service';
-import {catchError, EMPTY} from "rxjs";
+import {catchError, combineLatest, EMPTY, filter, map, Subject} from "rxjs";
 
 @Component({
   selector: 'pm-product-detail',
@@ -11,26 +11,45 @@ import {catchError, EMPTY} from "rxjs";
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ProductDetailComponent {
-  pageTitle = 'Product Detail';
-  errorMessage = '';
-  // productSuppliers: Supplier[] | null = null;
+
+  private errorMessageSubject = new Subject<string>();
+  errorMessage$ = this.errorMessageSubject.asObservable();
+
 
   product$ = this.productService.selectedProduct$
     .pipe(
       catchError(err => {
-        this.errorMessage = err;
+        this.errorMessageSubject.next(err);
         return EMPTY;
       })
     );
 
+  pageTitle$ = this.product$
+    .pipe(
+      map(p => p ? `Product detail for: ${p.productName}` : null)
+    )
+
   productSuppliers$ = this.productService.selectedProductSuppliers$
     .pipe(
       catchError(err => {
-        this.errorMessage = err;
+        this.errorMessageSubject.next(err);
         return EMPTY;
       }));
+
+
+  vm$ = combineLatest([
+    this.product$,
+    this.productSuppliers$,
+    this.pageTitle$
+  ]).pipe(
+    filter(([product]) => Boolean(product)),
+    map(([product, productSuppliers, pageTitle]) =>
+      ({product, productSuppliers, pageTitle}))
+  );
+
 
   constructor(private productService: ProductService) {
   }
 
 }
+
